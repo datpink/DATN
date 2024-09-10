996942a83ad39bcc8457a70598712bf208853fbd
datpink <datpink04@gmail.com>
2024-09-10 20:39:05 +0700

    migration

--- database/migrations/2024_09_09_175926_create_posts_table.php
+++ database/migrations/2024_09_09_175926_create_posts_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('content');
-            $table->string('image')->nullable();
             $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('title');
+            $table->text('summary');
             $table->softDeletes();
             $table->timestamps();
         });
--- database/migrations/2024_09_10_131850_create_post_part_table.php
+++ database/migrations/2024_09_10_131850_create_post_part_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('post_part', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->string('type');
+            $table->string('content');
+            $table->string('image');
+            $table->integer('order');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('post_part');
     }
 };
