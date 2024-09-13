4f967bfc89b82ffc726038c8b6c53106332bab02
datpink <datpink04@gmail.com>
2024-09-13 09:35:30 +0700

    more

--- database/migrations/2024_09_12_023916_create_product_variant_attributes_table.php
+++ database/migrations/2024_09_12_023916_create_product_variant_attributes_table.php
@@ -11,20 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post_part', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('post_id')->constrained()->onDelete('cascade');
-            $table->string('type');
-            $table->string('content');
-            $table->string('image');
-            $table->integer('order');
-            $table->softDeletes();
         Schema::create('product_variant_attributes', function (Blueprint $table) {
-            $table->id();
             $table->foreignId('product_variant_id')->constrained('product_variants')->onDelete('cascade');
             $table->foreignId('attribute_value_id')->constrained('attribute_values')->onDelete('cascade');
-
-            $table->timestamps();
+            $table->primary(['product_variant_id','attribute_value_id']);
         });
     }
 
@@ -35,8 +25,7 @@ return new class extends Migration
     {
 
         Schema::dropIfExists('post_part');
-========
+
         Schema::dropIfExists('product_variant_attributes');
->>>>>>>> Oai:database/migrations/2024_09_12_023916_create_product_variant_attributes_table.php
     }
 };
